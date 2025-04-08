bfe30f44cedfbd6beb4258acaa4f24112f10b1d4
Gabriele Panico <gabriele.panico@gmail.com>
2025-04-08 11:51:11 +0200

    version added: ### 0.2.38-rc2

--- publish.sh
+++ publish.sh
@@ -3,9 +3,18 @@ echo "version $version"
 
 npm i
 
+# Get curent branch name
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+remote_name=$(git config --get branch.$current_branch.remote)
+
+## Push commit to git
+git add .
+git commit -m "version added: ### $version"
+git push "$remote_name" "$current_branch"
+
 if [ "$version" != "" ]; then
     git tag -a "$version" -m "`git log -1 --format=%s`"
+    git push --tags
     echo "Created a new tag, $version"
-    git push origin --tags
     npm publish --access public
 fi
